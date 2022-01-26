34ab8187123fa5821e50d782ffe8d4680e3dc5ea
Vadym-Kuliabko <vadilatele@gmail.com>
2022-01-26 20:15:24 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  bodyColor: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', start);
+refs.stopBtn.addEventListener('click', stop);
+
+let changeColor = null;
+
+function start() {
+  changeColor = setInterval(randomColor => {
+    randomColor = getRandomHexColor();
+    refs.bodyColor.style.backgroundColor = randomColor;
+  }, 1000);
+  refs.startBtn.setAttribute('disabled', true);
+  refs.stopBtn.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function stop() {
+  clearInterval(changeColor);
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', true);
+}
